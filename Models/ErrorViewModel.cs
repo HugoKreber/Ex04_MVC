1e42b040231450ad4a2a9229f2f1d8a1238997a7
hugo.kreber <hugo.kreber@P230570>
2024-10-29 17:42:39 +0100

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ex04_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
